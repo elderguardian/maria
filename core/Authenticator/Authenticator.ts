8d7d228ef9bfb92d705b117c2632b8b0d0d989f2
elderguardian <katzenmaedchen@tutanota.com>
2023-09-13 19:53:03 +0200

    implemented auth

---
+++ core/Authenticator/Authenticator.ts
@@ -0,0 +1,94 @@
+import { IAuthenticator } from "./IAuthenticator";
+import { IAuthOptions } from "./IAuthOptions";
+
+export class Authenticator implements IAuthenticator {
+  private async getFirstAuthResponse(
+    authOptions: IAuthOptions
+  ): Promise<Response> {
+    const authLinkGetUrl = `https://login.schulportal.hessen.de/?i=${authOptions.schoolId}&`;
+
+    return await fetch(authLinkGetUrl, {
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+      },
+      redirect: "manual",
+      body: encodeURI(
+        `user2=${authOptions.username}` +
+          `&user=${authOptions.schoolId}.${authOptions.username}` +
+          `&password=${authOptions.password}`
+      ),
+      method: "POST",
+    });
+  }
+
+  private async getSecondAuthResponse(
+    firstAuthResponse: Response
+  ): Promise<Response> {
+    const rawCookieHeader = firstAuthResponse.headers.get("set-cookie");
+
+    if (!rawCookieHeader) {
+      throw new Error("Missing Cookie Header.");
+    }
+
+    const authLink = firstAuthResponse.headers.get("location");
+
+    if (!authLink) {
+      throw new Error("Second auth link is missing.");
+    }
+
+    return await fetch(authLink, {
+      redirect: "manual",
+      method: "GET",
+      headers: {
+        cookie: rawCookieHeader,
+      },
+    });
+  }
+
+  private async getThirdAuthResponse(
+    firstAuthResponse: Response,
+    secondAuthResponse: Response
+  ): Promise<Response> {
+    if (!secondAuthResponse.headers.has("location")) {
+      throw new Error("Third auth url is missing.");
+    }
+
+    const rawCookieHeader = firstAuthResponse.headers.get("set-cookie");
+
+    if (!rawCookieHeader) {
+      throw new Error("Missing Cookie Header.");
+    }
+
+    const nextUrl = secondAuthResponse.headers.get("location");
+
+    if (!nextUrl) {
+      throw new Error("Third auth url is missing.");
+    }
+
+    return fetch(nextUrl, {
+      method: "GET",
+      redirect: "manual",
+      headers: {
+        cookie: rawCookieHeader,
+      },
+    });
+  }
+
+  async getSessionId(authOptions: IAuthOptions): Promise<string> {
+    const firstAuth = await this.getFirstAuthResponse(authOptions);
+    const secondAuth = await this.getSecondAuthResponse(firstAuth);
+    const thirdAuth = await this.getThirdAuthResponse(firstAuth, secondAuth);
+
+    const rawCookieHeader = thirdAuth.headers.get("set-cookie");
+
+    if (!rawCookieHeader) {
+      throw new Error("Failed to get cookie header.");
+    }
+
+    return rawCookieHeader
+      .split("; ")
+      .filter((part) => part.includes("sid"))[0]
+      .split(", ")[1]
+      .split("=")[1];
+  }
+}
