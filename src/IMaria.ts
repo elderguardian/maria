494b3754ed37ec32fbd43e3b4f50012303ef3ccf
elderguardian <katzenmaedchen@tutanota.com>
2023-09-24 21:11:39 +0200

    added homework fetcher to fassade

--- src/IMaria.ts
+++ src/IMaria.ts
@@ -1,6 +1,8 @@
 import { IRepPlanFetchOptions } from "./foundation/RepPlan/IRepPlanFetchOptions";
 import { IRepPlanEntry } from "./foundation/RepPlan/IRepPlanEntry";
+import { IHomeworkEntry } from "./foundation/Homework/IHomeworkEntry";
 
 export interface IMaria {
   fetchRepPlan(options: IRepPlanFetchOptions): Promise<IRepPlanEntry[]>;
+  fetchHomework(options: IRepPlanFetchOptions): Promise<IHomeworkEntry[]>;
 }
